7d82188cdbea082ee42cc376ebbaa647555779eb
Dom-Owens-UoB <dom.owens@bristol.ac.uk>
2021-12-08 15:15:39 +0000

    paramlrpc example

---
+++ R/examples/paramlrpc.R
@@ -0,0 +1,11 @@
+#param.lrpc
+require(doParallel)
+require(lpSolve)
+set.seed(222)
+n <- 200
+p<- 100
+chi <- sim.factor.M1(n,p)
+xi <- sim.idio(n,p)
+sample.data <- chi$data + xi$data
+model <- fnets(sample.data, q=2, idio.method = "lasso")
+param.lrpc(model, sample.data, 1, n.cores = 1)
