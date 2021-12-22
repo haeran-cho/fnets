9905471460072d47a470978a74b21e7f7d964680
Dom-Owens-UoB <dom.owens@bristol.ac.uk>
2021-12-22 09:30:02 +0000

    M2 in examples

--- R/examples/fnets.R
+++ R/examples/fnets.R
@@ -1,7 +1,7 @@
 set.seed(222)
 n <- 200
 p<- 100
-chi <- sim.factor.M1(n,p)
+chi <- sim.factor.M2(n,p)
 xi <- sim.idio(n,p)
 sample.data <- chi$data + xi$data
 fnets(sample.data, q=2, idio.method = "lasso")
